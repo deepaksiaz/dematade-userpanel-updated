11408e37b036e1c7c8c9fd5b682a52b68197c239
prince <prince@domain.com>
2022-07-14 17:36:03 +0000

    Added CICD related files

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CONTAINER_NAME="dematade-trade-panel"
+IMAGE_NAME="demamtade-trade-panel"
+SRC=/home/ubuntu/dematade-userpanel-updated
+
+echo "=================="
+echo "|Pulling the code|"
+echo "=================="
+echo ""
+cd $SRC && git pull
+echo ""
+
+echo "======================="
+echo "|Building the codebase|"
+echo "======================="
+echo ""
+docker build -t $IMAGE_NAME .
+echo ""
+echo "==========================="
+echo "|DEPLOYING THE APPLICATION|"
+echo "==========================="
+echo ""
+
+
+docker rm -f $CONTAINER_NAME
+docker run -d -p 9090:80 --name $CONTAINER_NAME $IMAGE_NAME
+
+
+echo ""
+echo "==============================================="
+echo "| $IMAGE_NAME APPLICATION DEPLOYMENT COMPLETED|"
+echo "==============================================="
+echo ""
