ab2920d86546d8bc0d5d0a9544306b128dc2e032
deepaksiaz <101709977+deepaksiaz@users.noreply.github.com>
2022-07-15 14:53:47 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 let API_URL;
 switch (process.env.REACT_APP_ENVIRONMENT) {
   case "production":
-    API_URL = "https://api.dematadesolution.com";
+    API_URL = "http://test-api.dematadesolution.com";
     break;
   case "staging":
-    API_URL = "https://api.dematadesolution.com";
+    API_URL = "http://test-api.dematadesolution.com";
     break;
   default:
-    API_URL = "https://api.dematadesolution.com";
+    API_URL = "http://test-api.dematadesolution.com";
 }
 export { API_URL };
