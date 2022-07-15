51e0fc8c645f57cee5e0a21bb330d89d49ae645d
prateek.pareek <prateek.pareek@qtsolv.com>
2022-07-15 16:26:38 +0530

    url change

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import store from "./Redux/store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
