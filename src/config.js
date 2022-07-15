6cf60f26b8607407f586fcef4ef2918d3c6c966f
prateek.pareek <prateek.pareek@qtsolv.com>
2022-07-15 16:14:46 +0530

    url change

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ switch (process.env.REACT_APP_ENVIRONMENT) {
     API_URL = "https://api.dematadesolution.com";
     break;
   case "staging":
-    API_URL = "http://test-api.dematadesolution.com";
+    API_URL = "https://test-api.dematadesolution.com";
     break;
   default:
-    API_URL = "http://test-api.dematadesolution.com";
+    API_URL = "https://test-api.dematadesolution.com";
 }
 export { API_URL };
