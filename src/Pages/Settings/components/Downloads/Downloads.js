8dfd8b34acbeb6d93a37d9dc72a202e84518302b
deepaksiaz <101709977+deepaksiaz@users.noreply.github.com>
2022-07-17 01:12:40 +0530

    Update Downloads.js

--- src/Pages/Settings/components/Downloads/Downloads.js
+++ src/Pages/Settings/components/Downloads/Downloads.js
@@ -62,7 +62,7 @@ function Downloads(props) {
       <div
         onClick={() =>
           download(
-            "https://test-trade-panel.dematadesolution.com/Charting_Software/DeMatade.exe"
+            "https://www.dematadesolution.com/DeMatadechartingsoftwarefile/DeMatade%20Algo.exe"
           )
         }
         className="charting-download-btn"
